ff4e79a5ec50143ab4d0fb6e146487ff523ee008
cryinlaugh <cryinlaugh@gmail.com>
2018-12-04 16:00:35 +0800

    add tensor/basicOp/dlOp dirs

---
+++ src/tensor/tensor.cpp
@@ -0,0 +1,10 @@
+/*************************************************************************
+	> File Name: tensor.cpp
+	> Author: cryinlaugh 
+	> Mail: cryinlaugh@gmail.com 
+	> Created Time: 二 12/ 4 15:56:42 2018
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
---
+++ src/tensor/tensor.h
@@ -0,0 +1,11 @@
+/*************************************************************************
+	> File Name: tensor.h
+	> Author: cryinlaugh
+	> Mail: cryinlaugh@gmail.com
+	> Created Time: Tues. 12/ 4 15:53:19 2018
+ ************************************************************************/
+
+#ifndef _TENSOR_H
+#define _TENSOR_H
+
+#endif
