dc9666e43bcb71b0c05bb030789dd30566acb402
aagg008 <aagg008008@gmail.com>
2018-12-10 17:12:48 +0800

    Update the dotGen module to the latest version.

--- src/graphIR/IRNode.cpp
+++ src/graphIR/IRNode.cpp
@@ -11,7 +11,6 @@
 
 namespace swc {
 
-
 std::string IRNode::dotGen() {
 
 	std::vector<std::string> parentNodes;
@@ -20,7 +19,7 @@ std::string IRNode::dotGen() {
 	std::string str_tmp;
 	std::string str_total;
 	std::string thisNode;
-	std::string NodeEx[2] = { ";\n", " [shape = box];\n" };
+	std::string NodeInfo[2] = { " [shape = box];\n", ";\n" };
 
 	// init parentNodes
 	for (int i = 0; i < parentNum(); ++i) {
@@ -34,45 +33,18 @@ std::string IRNode::dotGen() {
 		childNodes.push_back(getChildNode(i)->name());
 	}
 
-	// init thisNode
-	thisNode = name();
-
 	// Generate dot codes.
 	str_total = str_total + "	// Generate one Node!\n";
 
-	// Generate Types (Tensor or OP)
-	for (int i = 0; i < parentNum(); ++i) {
+	// Generate the information of this Node
+	thisNode = name();
 
-		if (_nodeType == TENSOR_NODE) 
-			str_tmp = "	" + parentNodes[i] + NodeEx[0];
-		else if (_nodeType == OP_NODE)
-			str_tmp = "	" + parentNodes[i] + NodeEx[1];
+	if (_nodeType == TENSOR_NODE) 
+		str_tmp = "	" + thisNode + NodeInfo[0];
+	else if (_nodeType == OP_NODE)
+		str_tmp = "	" + thisNode + NodeInfo[1];
 
-		str_total = str_total + str_tmp;
-	}
-
-	// str_total = str_total + "\n";
-
-	// Generate Types (Tensor or OP)
-	for (int i = 0; i < childNum(); ++i) {
-
-		if (_nodeType == TENSOR_NODE) 
-			str_tmp = "	" + childNodes[i] + NodeEx[0];
-		else if (_nodeType == OP_NODE)
-			str_tmp = "	" + childNodes[i] + NodeEx[1];
-
-		str_total = str_total + str_tmp;
-	}
-
-	str_total = str_total + "\n";
-
-	// // Generate Father -> This
-	// for (int i = 0; i < parentNum(); ++i) {
-	// 	str_tmp   = "	" + parentNodes[i] + " -> " + thisNode + ";\n";
-	// 	str_total = str_total + str_tmp;
-	// }	
-
-	// str_total = str_total + "\n";
+	str_total = str_total + str_tmp;
 
 	// Generate -> Children
 	for (int i = 0; i < childNum(); ++i) {
