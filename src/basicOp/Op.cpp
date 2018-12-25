8ae6e15ca847dd4f6371b8c538caeab6e709af59
Hongkun Yu <staryhk@gmail.com>
2018-12-25 14:40:44 +0000

    add  INSTANTIATE for all class

--- src/basicOp/Op.cpp
+++ src/basicOp/Op.cpp
@@ -9,4 +9,19 @@
 
 namespace swc{
 
+template<typename Dtype>  
+bool Op<Dtype>::check(){
+  if(_nInputTensor != _nInput) return false;
+  if(_nOutputTensor != _nOutput) return false;
+  for(int i=0; i<_nInput; i++){
+    if(_inputTensors[i]->getNDim() != _inputNDims[i]) return false;
+  }
+  for(int i=0; i<_nOutput; i++){
+    if(_outputTensors[i]->getNDim() != _inputNDims[i]) return false;
+  }
+  return true;
+}
+
+INSTANTIATE_CLASS(Op);
+
 }
--- src/dlOp/dlOp.cpp
+++ src/dlOp/dlOp.cpp
@@ -5,4 +5,23 @@
 	> Created Time: 二 12/ 4 15:57:35 2018
  ************************************************************************/
 
+#include "dlOp.h"
 
+namespace swc {
+
+
+
+INSTANTIATE_CLASS(MatrixMatrixFCOp);
+INSTANTIATE_CLASS(MatrixTanhOp);
+INSTANTIATE_CLASS(MatrixSoftmaxOp);
+INSTANTIATE_CLASS(MatrixLogNegLossOp);
+
+
+INSTANTIATE_CLASS(VectorTanhOp);
+INSTANTIATE_CLASS(VectorSoftmaxOp);
+INSTANTIATE_CLASS(VectorLogNegLossOp);
+
+
+INSTANTIATE_CLASS(ScalarTanhOp);
+
+} //namespace swc
