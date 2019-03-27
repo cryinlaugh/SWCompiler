73d006a1ee6ca15eb83d5b0138ebfce532e8950b
waynehpc <44743868+waynehpc@users.noreply.github.com>
2019-03-27 22:09:07 +0800

    [Label]add Device label

--- src/pass/Label.h
+++ src/pass/Label.h
@@ -23,26 +23,29 @@ private:
     int _toLower;
 
     TensorInitType _initTypeLabel;
+    Device _dev;
 
 public:
-    Label() : _toLower(0), _initTypeLabel(TensorInitType::NONE){};
+    Label() : _toLower(0), _initTypeLabel(TensorInitType::NONE) {};
     virtual ~Label(){};
 
     void destroy(){
         this->~Label();
     };
 
-    void setNodeNameLabel(std::string s) { _nodeNameLabel = s; };
-    void setTypeNameLabel(std::string s) { _typeNameLabel = s; };
-    void setTensorInitTypeLabel(TensorInitType type) { _initTypeLabel = type; };
+    void setNodeNameLabel(std::string s) { _nodeNameLabel = s; }
+    void setTypeNameLabel(std::string s) { _typeNameLabel = s; }
+    void setTensorInitTypeLabel(TensorInitType type) { _initTypeLabel = type; }
+    void setDeviceLabel(DeviceType type, int id=0){ _dev.type = type; _dev.id = id; }
 
     void setLowerMark() { _toLower = 1;};
 
-    std::string getNodeNameLabel() const { return _nodeNameLabel; };
-    std::string getTypeNameLabel() const { return _typeNameLabel; };
-    TensorInitType getTensorInitTypeLabel() const { return _initTypeLabel; };
+    std::string getNodeNameLabel() const { return _nodeNameLabel; }
+    std::string getTypeNameLabel() const { return _typeNameLabel; }
+    TensorInitType getTensorInitTypeLabel() const { return _initTypeLabel; }
+    Device getDeviceLabel() const { return _dev; }
 
-    int getLowerMark() const { return _toLower;};
+    int getLowerMark() const { return _toLower;}
 
 
 };
