5ca858d1e86bea83d6c195528f3fa990c20afb66
Hongkun Yu <staryhk@gmail.com>
2019-10-04 21:18:25 +0800

    add optimized comSizeModel with sendrecive

--- src/op/comSizeModel.cpp
+++ src/op/comSizeModel.cpp
@@ -52,3 +52,24 @@ size_t  comSizeModel2(size_t size, COMM_TYPE type, Config& config)
     }
 }
 
+//The third type of cost model:
+//  relative communication time with mpi-utilization model and optimization
+size_t  comSizeModel2(size_t size, COMM_TYPE type, Config& config)
+{
+    int degree = config.mpi_size;
+
+
+    switch(type) {
+        case RECV_SEND: return size / degree / degree * (degree - 1) * 2;
+        case REDUCE_SEND: return size * log(degree)/log(2) + size;
+        case RECV_BCAST: return size * log(degree)/log(2) + size;
+        case REDUCE_BCAST: return (config.net_topo == RING_NET ? size : 2 * size * log(degree)/log(2));
+        case SELF_CP: return 0;
+        case SCATTER: return size;
+        case GATHER: return size;
+        case REDUCE: return size * log(degree)/log(2);
+        case BCAST: return size * log(degree)/log(2);
+        default: assert("invalid communication type\n"); 
+    }
+}
+
