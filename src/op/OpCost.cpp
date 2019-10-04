8993e0fcb6452eaf4237275e18cac80856277b59
waynehpc <44743868+waynehpc@users.noreply.github.com>
2019-10-04 15:02:00 +0000

    [Performance model] print communication trace as data size and normalized communication size

--- src/op/OpCost.cpp
+++ src/op/OpCost.cpp
@@ -40,9 +40,15 @@ size_t ScatterOp::getCost(OpNode *node, Config& config){
     auto *from = (TensorNode*)node->getParentNode(0);
     size_t size = from->getTensor()->getSizeInBytes(); 
 
+    int axis = this->axis_;
     //  i: master send size/p to each worker
-    // -2: master broadcast size to all workers
-    return comSizeModel(size, SCATTER, config);
+    // -1: master broadcast size to all workers
+    if(axis == -1)
+        return comSizeModel(size, BCAST, config);
+    if(axis >= 0)
+        return comSizeModel(size, SCATTER, config);
+
+    return 0;
 }
 
 // axis(strategy): -1 rep , i scatter 
@@ -50,6 +56,10 @@ size_t ScatterOp::getSimCost(size_t bytes, Config& config, int axis) {
     (void)config;
     (void)axis;
     return comSizeModel(bytes, SCATTER, config);
+    if(axis == -1)
+        return comSizeModel(bytes, BCAST, config);
+    if(axis >= 0)
+        return comSizeModel(bytes, SCATTER, config);
 }
 
 std::string ScatterOp::getCostTrace(OpNode *node, Config& config){
@@ -59,15 +69,17 @@ std::string ScatterOp::getCostTrace(OpNode *node, Config& config){
     auto *from = (TensorNode*)node->getParentNode(0);
     auto *to = (TensorNode*)node->getChildNode(0);
     
-    size_t  comm = getCost(node, config);
+    size_t  comm = from->getTensor()->getSizeInBytes();
+
+    size_t  norm_comm = getCost(node, config); 
 
     int axis = this->axis_;
 
     if(axis == -1)
-        stream << name << " " << "Bcast" << " " << comm << " ";
+        stream << name << " " << "Bcast" << " " << comm << " " << norm_comm << " ";
 
     if(axis >= 0)
-        stream << name << " " << "Send" << " " << comm << " ";
+        stream << name << " " << "Send" << " " << comm << " " << norm_comm << " ";
 
     stream << dumpDims(from->getDims()) << " "
         << dumpDims(to->getDims()) << " "
@@ -87,21 +99,25 @@ size_t GatherOp::getCost(OpNode *node, Config& config) {
 }
 
 std::string GatherOp::getCostTrace(OpNode *node, Config& config) {
+    (void)config;
+
     std::ostringstream stream;
 
     std::string name = node->name();
     auto *from = (TensorNode*)node->getParentNode(0);
     auto *to = (TensorNode*)node->getChildNode(0);
 
-    size_t  comm = getCost(node, config);
+    size_t  comm = to->getTensor()->getSizeInBytes();
+
+    size_t  norm_comm = getCost(node, config); 
 
     int axis = this->axis_;
 
     if(axis == -2)
-    stream << name << " " << "Reduce"<< " " << comm << " ";
+    stream << name << " " << "Reduce"<< " " << comm << " " << norm_comm << " ";
 
     if(axis >= 0)
-        stream << name << " " << "Recv" << " " << comm << " ";
+        stream << name << " " << "Recv" << " " << comm << " " << norm_comm << " ";
 
     stream << dumpDims(from->getDims()) << " "
         << dumpDims(to->getDims()) << " "
@@ -123,7 +139,7 @@ size_t TransformOp::getCost(OpNode *node, Config& config) {
 
     int pre = this->preAxis_;   // pre {i, -1, -2}
     int post = this->postAxis_; // post{i, -1}
-    int p = this->degree_;
+    // int p = this->degree_;
 
     size_t comm = 0;
 
@@ -178,15 +194,17 @@ std::string TransformOp::getCostTrace(OpNode *node, Config& config) {
 
     stream.str("");
 
+    size_t norm_comm = getCost(node, config);
+
     // i->j: each work send and recv size in total (pieces of data) 
     if(pre>=0 && post>=0) {
-        stream << name << " " << "SendRecv " << size << " " << suffix << "\n";
+        stream << name << " " << "SendRecv " << size << " " << norm_comm << " " << suffix << "\n";
     }
 
     // i->j: master recv size, then broadcast size
     if(pre>=0 && post==-1) {
-        stream << name << " " << "Recv " << size << "\n"
-            << name << " " << "Bcast " << size << " " << suffix << "\n";
+        stream << name << " " << "Recv " << size << " " << "0" << " " << suffix << "\n"
+            << name << " " << "Bcast " << size << " " << norm_comm << " " << suffix << "\n";
             
     }
 
@@ -194,16 +212,15 @@ std::string TransformOp::getCostTrace(OpNode *node, Config& config) {
     //  or b.each worker reduce its own part(not continuous)
     //  we took a but comm has two parts
     if(pre==-2 && post>=0) {
-        stream << name << " " << "Reduce " << size << " " << suffix << "\n"
-            << name << " " << "Send " << size << " " << suffix << "\n";
+        stream << name << " " << "Reduce " << size << " " << "0" << " " << suffix << "\n"
+            << name << " " << "Send " << size << " " << norm_comm << " " << suffix << "\n";
     }
 
     // -2->-1 a. allreduce size
     // or b. master reduce size, then broadcast size
     if(pre==-2 && post==-1) {
-        stream << name << " " << "Reduce " << size << " " << suffix << "\n"
-            << name << " " << "Bcast " << size << " " << suffix << "\n";
-
+        stream << name << " " << "Reduce " << size << " " << "0" << " " << suffix << "\n"
+            << name << " " << "Bcast " << size << " " << norm_comm << " " << suffix << "\n";
         // stream << name << " " << "AllReduce," << size << "\n";
     }
 
@@ -211,7 +228,7 @@ std::string TransformOp::getCostTrace(OpNode *node, Config& config) {
     // if continuous, just let post pointer reference to its own part
     // if strided, just memcpy or self sendrecv is ok 
     if(pre==-1 && post>=0) {
-        stream << name << " " << "SelfSR " << size/p << " " << suffix << "\n";
+        stream << name << " " << "SelfSR " << size/p << " " << norm_comm << " " << suffix << "\n";
     }
     
     return stream.str();
@@ -273,9 +290,11 @@ std::string ReduceOp::getCostTrace(OpNode *node, Config& config) {
     auto *from = (TensorNode*)node->getParentNode(0);
     auto *to = (TensorNode*)node->getChildNode(0);
 
-    size_t  comm = getCost(node, config);
+    size_t comm = to->getTensor()->getSizeInBytes(); 
 
-    stream << name << " " << "Reduce"<< " " << comm << " ";
+    size_t norm_comm = getCost(node, config);
+
+    stream << name << " " << "Reduce"<< " " << comm << " " << norm_comm << " ";
 
 
     stream << dumpDims(from->getDims()) << " "
@@ -296,7 +315,7 @@ size_t BroadcastOp::getCost(OpNode *node, Config& config) {
     auto *to = (TensorNode*)node->getChildNode(0);
     size_t size = to->getTensor()->getSizeInBytes(); 
 
-    // -2: master reduce size to all workers
+    // -1: master bcast size to all workers
     return comSizeModel(size, BCAST, config);
 }
 
@@ -309,9 +328,11 @@ std::string BroadcastOp::getCostTrace(OpNode *node, Config& config) {
     auto *from = (TensorNode*)node->getParentNode(0);
     auto *to = (TensorNode*)node->getChildNode(0);
 
-    size_t  comm = getCost(node, config);
+    size_t comm = to->getTensor()->getSizeInBytes(); 
 
-    stream << name << " " << "Broadcast"<< " " << comm << " ";
+    size_t  norm_comm = getCost(node, config); 
+
+    stream << name << " " << "Bcast"<< " " << comm << " " << norm_comm << " ";
 
 
     stream << dumpDims(from->getDims()) << " "
